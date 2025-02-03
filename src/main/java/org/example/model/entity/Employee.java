ebadd206765152e76b753c00a6050942ceaca133
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-02-03 13:26:08 +0100

    add Employee class

---
+++ src/main/java/org/example/model/entity/Employee.java
@@ -0,0 +1,23 @@
+package org.example.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @OneToOne
+    private ConferenceRoom conferenceRoom;
+
+
+}
