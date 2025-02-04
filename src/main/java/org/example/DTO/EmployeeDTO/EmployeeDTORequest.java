2a8857030cdec5550736affeb11c2fd4ca661928
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-02-04 12:02:44 +0100

    add EmployeeDTO request and response

---
+++ src/main/java/org/example/DTO/EmployeeDTO/EmployeeDTORequest.java
@@ -0,0 +1,14 @@
+package org.example.DTO.EmployeeDTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmployeeDTORequest {
+
+    private String firstName;
+    private String lastName;
+}
---
+++ src/main/java/org/example/DTO/EmployeeDTO/EmployeeDTOResponse.java
@@ -0,0 +1,14 @@
+package org.example.DTO.EmployeeDTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmployeeDTOResponse {
+
+    private String firstName;
+    private String lastName;
+}
