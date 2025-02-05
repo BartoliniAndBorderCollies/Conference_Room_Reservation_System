a1bebc8460e1ec3cd2811db49a06445a26e51744
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-02-05 13:08:48 +0100

    rename of Main class to ConferenceRoomReservationApplication

---
+++ src/main/java/org/example/ConferenceRoomReservationApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceRoomReservationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceRoomReservationApplication.class);
+    }
+}
