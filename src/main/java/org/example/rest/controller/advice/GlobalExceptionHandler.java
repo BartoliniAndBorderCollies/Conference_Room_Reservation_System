70eabc60f792fc6ef1782322c61dfcbe72fe5151
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-02-05 15:25:53 +0100

    add GlobalExceptionHandler

---
+++ src/main/java/org/example/rest/controller/advice/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package org.example.rest.controller.advice;
+
+import org.example.DTO.ResponseDTO;
+import org.example.exception.NotFoundInDatabaseException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundInDatabaseException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseDTO handleNotFoundInDatabaseException(NotFoundInDatabaseException e) {
+        return new ResponseDTO(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
