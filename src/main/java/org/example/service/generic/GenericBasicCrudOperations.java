04c696e1640f8e4ee529db926cdd27988f46d4c4
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-02-05 15:18:23 +0100

    add throws signature to deleteById() in GenericBasicCrudOperations

--- src/main/java/org/example/service/generic/GenericBasicCrudOperations.java
+++ src/main/java/org/example/service/generic/GenericBasicCrudOperations.java
@@ -1,10 +1,11 @@
 package org.example.service.generic;
 
 import org.example.DTO.ResponseDTO;
+import org.example.exception.NotFoundInDatabaseException;
 
 public interface GenericBasicCrudOperations<T, K, ID> {
 
     T create(K object);
-    ResponseDTO deleteById(ID id);
+    ResponseDTO deleteById(ID id) throws NotFoundInDatabaseException;
 
 }
