9453c8c9298a073b4772737b9b247ba1246c278b
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-02-05 15:02:13 +0100

    refactor of GenericBasicCrudOperations interface -> add id to delete method

--- src/main/java/org/example/service/generic/GenericBasicCrudOperations.java
+++ src/main/java/org/example/service/generic/GenericBasicCrudOperations.java
@@ -2,9 +2,9 @@ package org.example.service.generic;
 
 import org.example.DTO.ResponseDTO;
 
-public interface GenericBasicCrudOperations<T, K> {
+public interface GenericBasicCrudOperations<T, K, ID> {
 
     T create(K object);
-    ResponseDTO delete(K object);
+    ResponseDTO deleteById(ID id);
 
 }
