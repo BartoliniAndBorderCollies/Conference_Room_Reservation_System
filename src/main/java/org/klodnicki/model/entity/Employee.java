0b61132f152e4e48a87de5602eacb965e889a84f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-02-06 12:01:02 +0100

    add @AllArgsConstructor to Employee

--- src/main/java/org/klodnicki/model/entity/Employee.java
+++ src/main/java/org/klodnicki/model/entity/Employee.java
@@ -1,6 +1,7 @@
 package org.klodnicki.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
