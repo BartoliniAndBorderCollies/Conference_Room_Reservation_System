6a38ec84c14759d286d785543612c880c0902f28
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-02-06 12:01:36 +0100

    add id and reservation fields to EmployeeDTOResponse

--- src/main/java/org/klodnicki/DTO/EmployeeDTO/EmployeeDTOResponse.java
+++ src/main/java/org/klodnicki/DTO/EmployeeDTO/EmployeeDTOResponse.java
@@ -1,14 +1,21 @@
 package org.klodnicki.DTO.EmployeeDTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.klodnicki.model.entity.Reservation;
+
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTOResponse {
 
+    private Long id;
     private String firstName;
     private String lastName;
+    private List<Reservation> reservations;
 }
