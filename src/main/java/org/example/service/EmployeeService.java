2485c47f02b2992fcac547a7d747b746b26f7f15
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-02-06 09:51:54 +0100

    add deleteById() in EmployeeService

--- src/main/java/org/example/service/EmployeeService.java
+++ src/main/java/org/example/service/EmployeeService.java
@@ -4,18 +4,21 @@ import lombok.AllArgsConstructor;
 import org.example.DTO.EmployeeDTO.EmployeeDTORequest;
 import org.example.DTO.EmployeeDTO.EmployeeDTOResponse;
 import org.example.DTO.ResponseDTO;
+import org.example.exception.NotFoundInDatabaseException;
 import org.example.model.entity.Employee;
 import org.example.repository.EmployeeRepository;
 import org.example.service.generic.GenericBasicCrudOperations;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class EmployeeService implements GenericBasicCrudOperations<EmployeeDTOResponse, EmployeeDTORequest> {
+public class EmployeeService implements GenericBasicCrudOperations<EmployeeDTOResponse, EmployeeDTORequest, Long> {
 
     private final EmployeeRepository employeeRepository;
     private final ModelMapper modelMapper;
+    private static final String DELETE_MESSAGE = "Record has been successfully deleted!";
 
     @Override
     public EmployeeDTOResponse create(EmployeeDTORequest employeeDTORequest) {
@@ -27,8 +30,11 @@ public class EmployeeService implements GenericBasicCrudOperations<EmployeeDTORe
     }
 
     @Override
-    public ResponseDTO delete(EmployeeDTORequest object) {
-        return null;
-    }
+    public ResponseDTO deleteById(Long id) throws NotFoundInDatabaseException {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new NotFoundInDatabaseException(Employee.class));
 
+        employeeRepository.delete(employee);
+
+        return new ResponseDTO(DELETE_MESSAGE, HttpStatus.OK);
+    }
 }
