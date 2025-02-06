3405632cac4379674dc73ff8d95399fd44dc5428
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-02-06 10:09:31 +0100

    refactor of GenericBasicCrudOperations -> modification deleteById and add findById()

--- src/main/java/org/example/service/generic/GenericBasicCrudOperations.java
+++ src/main/java/org/example/service/generic/GenericBasicCrudOperations.java
@@ -1,11 +1,10 @@
 package org.example.service.generic;
 
-import org.example.DTO.ResponseDTO;
 import org.example.exception.NotFoundInDatabaseException;
 
 public interface GenericBasicCrudOperations<T, K, ID> {
 
     T create(K object);
-    ResponseDTO deleteById(ID id) throws NotFoundInDatabaseException;
-
+    void deleteById(ID id) throws NotFoundInDatabaseException;
+    T findById(ID id) throws NotFoundInDatabaseException;
 }
