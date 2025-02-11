157afc71fdfdaa3e01f845cbfc440270f46e081f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-02-11 12:04:02 +0100

    add ConferenceRoomDTOResponse class

---
+++ src/main/java/org/klodnicki/DTO/ConferenceRoomDTO/ConferenceRoomDTOResponse.java
@@ -0,0 +1,22 @@
+package org.klodnicki.DTO.ConferenceRoomDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.klodnicki.DTO.ReservationDTOResponse;
+import org.klodnicki.model.Equipment;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConferenceRoomDTOResponse {
+
+    private Long id;
+    private int personLimit;
+    private List<Equipment> equipmentList;
+    private List<ReservationDTOResponse> reservations;
+}
