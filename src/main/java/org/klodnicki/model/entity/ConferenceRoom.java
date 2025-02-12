fb78214efb55ea9ffdeb21800e690ea2a6246872
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-02-12 11:29:59 +0100

    refactor ConferenceRoom to hold Equipment as entity (not enum)

--- src/main/java/org/klodnicki/model/entity/ConferenceRoom.java
+++ src/main/java/org/klodnicki/model/entity/ConferenceRoom.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.klodnicki.model.Equipment;
 
 import java.util.List;
 
@@ -19,10 +18,7 @@ public class ConferenceRoom {
     private Long id;
     private String name;
     private int personLimit;
-    @ElementCollection
-    @CollectionTable(name = "conference_room_equipment", joinColumns = @JoinColumn(name = "conference_room_id"))
-    @Column(name = "equipment")
-    @Enumerated(EnumType.STRING)
+    @OneToMany(mappedBy = "conferenceRoom")
     private List<Equipment> equipmentList;
     @OneToMany(mappedBy = "conferenceRoom", cascade = CascadeType.ALL, orphanRemoval = true) //orhpan removal -> jak usunę rezerwację z listy to zostanie ona usunieta również z bazy danych
     private List<Reservation> reservations;
