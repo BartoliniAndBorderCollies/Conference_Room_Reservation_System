49c39ced86da4bebe60965bb063b34b419ab20b0
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2025-02-13 11:02:27 +0100

    add create() in ConferenceRoomController

--- src/main/java/org/klodnicki/rest/controller/ConferenceRoomController.java
+++ src/main/java/org/klodnicki/rest/controller/ConferenceRoomController.java
@@ -1,13 +1,11 @@
 package org.klodnicki.rest.controller;
 
 import lombok.AllArgsConstructor;
+import org.klodnicki.DTO.ConferenceRoomDTO.ConferenceRoomDTORequest;
 import org.klodnicki.DTO.ConferenceRoomDTO.ConferenceRoomDTOResponse;
 import org.klodnicki.exception.NotFoundInDatabaseException;
 import org.klodnicki.service.ConferenceRoomService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/conference-room")
@@ -15,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConferenceRoomController extends ApiController{
 
     private final ConferenceRoomService conferenceRoomService;
+
+    @PostMapping
+    ConferenceRoomDTOResponse create(@RequestBody ConferenceRoomDTORequest conferenceRoomDTORequest) {
+        return conferenceRoomService.create(conferenceRoomDTORequest);
+    }
 
     @GetMapping("/{id}")
     ConferenceRoomDTOResponse findById(@PathVariable Long id) throws NotFoundInDatabaseException {
